9de7d44462e54805f630efe3c4d223182a45f850
baldpixels <baldpixels@gmail.com>
2017-10-30 00:55:19 -0500

    u.rand() is a game

---
+++ info.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<?php include 'style/cssLinks.php';?>
+		<link rel="stylesheet" type="text/css" href="style/navTop.css">
+		<link rel="stylesheet" type="text/css" href="style/content.css">
+		<?php include 'style/fonts.php';?>
+		<title>pwrdoor off</title>
+	</head>
+
+	<body>
+
+		<div id="container"> <!--fixed position-->
+			<figure> <!--absolute position-->
+				<a href="index.php"><img id="logoTop" src="images/logo.png" onmouseover="this.src='images/logo_hover.png';" onmouseout="this.src='images/logo.png';" alt="pwrdoor.com"/></a>
+			</figure>
+
+			<?php include 'nav.php';?> <!--absolute position-->
+
+			<main> <!--absolute position-->
+				<section>
+					
+				</section>
+			</main>
+
+			<div id="footer"><?php include 'footer.php';?></div> <!--absolute position-->
+		</div>
+
+	</body>
+
+</html>
--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
-<a href="movies.php"><img id="topNav" src='images/movies.png' onmouseover="this.src='images/movies_hover.png';" onmouseout="this.src='images/movies.png';"></img></a>
+<a href="movies.php"><img id="topNav" src='images/movies.png' onmouseover="this.src='images/movies_ho.png';" onmouseout="this.src='images/movies.png';"></img></a>
 
-<a href="graphics.php"><img id="bottomNav" src='images/graphics.gif' onmouseover="this.src='images/graphics.gif';" onmouseout="this.src='images/graphics.gif';"></img></a>
+<a href="graphics.php"><img id="bottomNav" src='images/graphics.gif' onmouseover="this.src='images/graphics_hover.gif';" onmouseout="this.src='images/graphics.gif';"></img></a>
 
-<a href="rand.php"><img id="rightNav" src='images/rand().gif'></img></a>
+<a href="rand.php"><img id="rightNav" src='images/rand().gif' onmouseover="this.src='images/rand()_hover.gif';" onmouseout="this.src='images/rand().gif';"></img></a>
 
-<a href="about.php"><img id="leftNav" src='images/xinxi.png' onmouseover="this.src='images/xinxi_hover.png';" onmouseout="this.src='images/xinxi.png';"></img></a>
+<a href="info.php"><img id="leftNav" src='images/xinxi.png' onmouseover="this.src='images/xinxi_hover.png';" onmouseout="this.src='images/xinxi.png';"></img></a>
